8fc9d7569202c69ca16a4cc1889ab0f02b798df6
sfkse <sfkse5591@gmail.com>
2021-12-02 20:43:47 +0200

    Update firebase config

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -3,7 +3,7 @@ import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyCPJpQq6RyCsWs4wY-ln1csWLSmxvnFcVE',
+    apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
