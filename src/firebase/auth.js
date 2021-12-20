b995062263db5d084d9e621dcaf7babea65ab2fe
sfkse <sfkse5591@gmail.com>
2021-12-20 23:23:43 +0200

    Update file

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -12,7 +12,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 const auth = getAuth();
 
 export const signUpUser = (values, setUserAuth, setError, history) => {
